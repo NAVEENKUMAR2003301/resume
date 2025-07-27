f3910a6c7ad3ece3d5ad7a7c54f23a3b7ce4d033
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2025-07-27 23:27:18 +0530

    contact

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+//about page
 
+
+setInterval(()=>{
+    let allBox = document.querySelectorAll(".box");
+    let randomNum = Math.floor(Math.random() * allBox.length);
+    let randomBox = allBox[randomNum];
+
+    randomBox.classList.add("animateMe");
+setTimeout(()=>{
+    randomBox.classList.remove("animateMe");
+
+},4000)
+
+},5000)
